4c0adcf205de09dc777edc0b9c23fd5267df0c92
obito1903 <sam200133@gmail.com>
2024-06-11 16:53:30 +0200

    use sub dir to store config

--- app/main.py
+++ app/main.py
@@ -25,48 +25,48 @@ def get_mail_csv(service):
 
     # GET CAR-COPILOT LABEL MAIL LISTS
     mails_from_label = service.users().messages().list(userId="me", labelIds = ['UNREAD','Label_6410322614978152058']).execute()
-    
+
     # AVOID ERROR IF NO MAIL IN LABEL
     if(mails_from_label["resultSizeEstimate"] > 0) :
-      
+
       # GET THE LAST MAIL
       mail_id = mails_from_label["messages"][0]["id"]
       specific_mail = service.users().messages().get(userId="me", id=mail_id).execute()
-    
+
       # [FINAL] GET THE CAR NAME
       car_name = specific_mail["snippet"]
       # html_car_name = base64.b64decode(specific_mail["payload"]["parts"][0]["parts"][1]["body"]["data"], altchars=b'-_')
       # car_name = html_car_name.decode('utf-8') #.split("<")[2].split(">")[1]
-      
+
       # [FINAL] GET THE EXPEDITOR
       headers = specific_mail["payload"]["headers"]
       mail_sender = "Unknown"
       for k in headers :
         if (k["name"] == 'Return-Path') :
           mail_sender = k["value"].split("<")[1].split(">")[0]
-          
+
       # GET THE CSV FILE NAME
       file_name = specific_mail["payload"]["parts"][1]["filename"]
-      
+
       # GET THE CSV DATA
       att_id = specific_mail["payload"]["parts"][1]["body"]["attachmentId"]
       csv_file = service.users().messages().attachments().get(userId="me", messageId=mail_id, id=att_id).execute()
       csv_data = base64.b64decode(csv_file["data"], altchars=b'-_')
-      
+
       # WRITE THE CSV DATA IN LOCAL CSV
       # f = open("influx_data.csv", "w")
       # f.write(csv_data.decode('utf-8'))
       # f.close()
-      
+
       # READ THE MESSAGE WHEN THREATED
       service.users().messages().modify(userId="me", id=mail_id, body={"removeLabelIds": ['UNREAD']}).execute()
 
       return send_infos(car_name, mail_sender, file_name, csv_data)
-      
+
     return("No unread mails in 'car-copilot' label")
-    
-    
-    
+
+
+
 
 def send_infos(car_name, mail_sender, file_name, csv_data):
 
@@ -85,11 +85,11 @@ def send_infos(car_name, mail_sender, file_name, csv_data):
   response = requests.request("POST", url, headers=headers, data=payload, files=files)
 
   print(response.text)
-  
+
   return("Data sent to DB")
-  
-  
-  
+
+
+
 
 def main():
   """Shows basic usage of the Gmail API.
@@ -100,24 +100,24 @@ def main():
   # created automatically when the authorization flow completes for the first
   # time.
   if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    creds = Credentials.from_authorized_user_file("config/token.json", SCOPES)
   # If there are no (valid) credentials available, let the user log in.
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
+          "config/credentials.json", SCOPES
       )
       creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
-    with open("token.json", "w") as token:
+    with open("config/token.json", "w") as token:
       token.write(creds.to_json())
 
   try:
     # Call the Gmail API
     service = build("gmail", "v1", credentials=creds)
-    
+
     killer = GracefulKiller()
     while not killer.kill_now:
       print(get_mail_csv(service))
