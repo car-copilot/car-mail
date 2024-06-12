655222bd0b00f987b499d3e241acf86949a6443a
obito1903 <sam200133@gmail.com>
2024-06-13 00:17:26 +0200

    disable browser

--- app/main.py
+++ app/main.py
@@ -109,7 +109,7 @@ def main():
       flow = InstalledAppFlow.from_client_secrets_file(
           "config/credentials.json", SCOPES
       )
-      creds = flow.run_local_server(port=0)
+      creds = flow.run_local_server(port=0,open_browser=False)
     # Save the credentials for the next run
     with open("config/token.json", "w") as token:
       token.write(creds.to_json())
